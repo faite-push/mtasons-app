3d7f18796c7e16bc08857ae75dd1a7d6851b8e4a
faite-push <fightxs176@gmail.com>
2026-01-22 00:57:54 -0300

    Atualizando o favicon do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/favicon.png',
+        url: '/favicon.ico',
         type: 'image/png',
       },
     ],
